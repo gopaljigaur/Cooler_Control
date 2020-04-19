095e1402eb16dbb660c31688e8963f9fcff0de0d
Gopalji Gaur <gopaljigaur@gmail.com>
2020-04-19 23:57:44 +0530

    Added separate boxes for timer setting

--- Cooler Control CRX/popup.js
+++ Cooler Control CRX/popup.js
@@ -3,6 +3,8 @@ window.onload = function() {
   var switchOffButton = document.getElementById('switchOff');
   var homeWiFi = document.getElementById('home');
   var coolerWiFi = document.getElementById('cooler');
+  var hours = document.getElementById('hours');
+  var minutes = document.getElementById('minutes');
   var seconds = document.getElementById('seconds');
   var setTimer = document.getElementById('setTimer');
   var server = 'http://192.168.15.150';
@@ -48,44 +50,82 @@ window.onload = function() {
       switchOnButton.disabled = false;
       switchOffButton.disabled = true;
     });
+  hours.addEventListener('click',function(){
+  		setTimer.disabled=false;
+  });
+  minutes.addEventListener('click',function(){
+  		setTimer.disabled=false;
+  });
   seconds.addEventListener('click',function(){
   		setTimer.disabled=false;
   });
   setTimer.addEventListener('click',function(){
-  	if(seconds.value==''||seconds.value=='0'){
+  	if((hours.value==''||hours.value=='0')&&(minutes.value==''||minutes.value=='0')&&(seconds.value==''||seconds.value=='0')){
   		alert('I am not an idiot like you');
   	}
   	else{
-
+  		var c = 3;
+  		if(hours.value==''){
+  			hours.value='0';
+  			c=c-1;
+  		}
+  		if(minutes.value==''){
+  			minutes.value='0';
+  			c=c-1;
+  		}
+  		if(seconds.value==''){
+  			seconds.value='0';
+  			c=c-1;
+  		}
+  		var value_to_send = hours.value*3600 + minutes.value*60 +seconds.value;
   		d = document;
   		var f = d.createElement('form');
-  		f.action = server.concat('/specificArgs?secs=',seconds.value);
+  		f.action = server.concat('/specificArgs?secs=',value_to_send);
   		f.method = 'post';
     	d.body.appendChild(f);
     	f.submit();
     	d.body.removeChild(f);
     	setTimer.disabled = true;
-    	var hrs = Math.floor(seconds.value/3600);
-    	var min = Math.floor((seconds.value-hrs*3600)/60);
-
-   		var hrs_phrase='';
-   		var min_phrase='';
-   		
-   		if(hrs==1){
+    	hrs_phrase='';
+    	min_phrase='';
+    	sec_phrase='';
+   		if(hours.value==1){
    			hrs_phrase='1 hour ';
    		}
-   		if(min==1){
+   		if(minutes.value==1){
    			min_phrase=' 1 minute ';
    		}
-
-   		if(hrs>1){
-   			hrs_phrase=hrs.toString().concat(' hours ');
-   		}
-   		if(min>1){
-   			min_phrase=min.toString().concat(' minutes ');
+   		if(seconds.value==1){
+   			sec_phrase=' 1 second';
    		}
 
-    	alert('Timer set for '.concat(hrs_phrase,min_phrase));
+   		if(hours.value>1){
+   			hrs_phrase=(hours.value).concat(' hours ');
+   		}
+   		if(minutes.value>1){
+   			min_phrase=(minutes.value).concat(' minutes ');
+   		}
+   		if(seconds.value>1){
+   			sec_phrase=(seconds.value).concat(' seconds');
+   		}
+   		var alert_str = 'Timer set for '.concat(hrs_phrase,min_phrase,sec_phrase);
+   		//alert(alert_str);
+    	alert_str = alert_str.trim();
+    	if(c==1){
+    		alert_str=alert_str.concat('.');
+    	}
+    	if(c==2){
+    		var arr = alert_str.split(" ");
+    		alert_str = arr[0].concat(" ",arr[1]," ",arr[2]," ",arr[3].concat(" ",arr[4]," and ",arr[5]," ",arr[6],"."));
+    	}
+    	if(c==3){
+    		var arr = alert_str.split(" ");
+    		alert_str = arr[0].concat(" ",arr[1]," ",arr[2]," ",arr[3].concat(" ",arr[4],", ",arr[5]," ",arr[6]," and ",arr[7]," ",arr[8],"."));
+    	}
+    	
+    	alert(alert_str);
+    	hours.value='';
+    	minutes.value='';
     	seconds.value='';
 		}
   	});
