116b2cc5c0eeffbb63eb9693dd2ccdd50706fe30
Gopalji Gaur <gopaljigaur@gmail.com>
2020-04-19 19:53:20 +0530

    Added timer feature to extension

--- Cooler Control CRX/popup.js
+++ Cooler Control CRX/popup.js
@@ -1,23 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
   var switchOnButton = document.getElementById('switchOn');
   var switchOffButton = document.getElementById('switchOff');
   var homeWiFi = document.getElementById('home');
   var coolerWiFi = document.getElementById('cooler');
+  var seconds = document.getElementById('seconds');
+  var setTimer = document.getElementById('setTimer');
   var server = 'http://192.168.15.150'
-  var firstTime = true;
   homeWiFi.disabled = true;
-
+  setTimer.disabled = true;
   homeWiFi.addEventListener('click', function() {
 
       server = 'http://192.168.15.150';
       console.log(server.concat(' selected'));
       homeWiFi.disabled = true;
       coolerWiFi.disabled = false;
-      if(firstTime){
-      switchOnButton.disabled = false;
-      switchOffButton.disabled = false;
-      firstTime=false;
-    }
     });
   coolerWiFi.addEventListener('click', function() {
 
@@ -25,11 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log(server.concat(' selected'));
       homeWiFi.disabled = false;
       coolerWiFi.disabled = true;
-      if(firstTime){
-      switchOnButton.disabled = false;
-      switchOffButton.disabled = false;
-      firstTime=false;
-    }
     });
   switchOnButton.addEventListener('click', function() {
 
@@ -56,6 +47,25 @@ document.addEventListener('DOMContentLoaded', function() {
       switchOnButton.disabled = false;
       switchOffButton.disabled = true;
     });
+  seconds.addEventListener('click',function(){
+  		setTimer.disabled=false;
+  });
+  setTimer.addEventListener('click',function(){
+  	if(seconds.value==''||seconds.value=='0'){
+  		alert('I am not an idiot like you');
+  	}
+  	else{
+  	d = document;
+  	var f = d.createElement('form');
+  	f.action = server.concat('/specificArgs?secs=',seconds.value);
+  	f.method = 'post';
+    d.body.appendChild(f);
+    f.submit();
+    d.body.removeChild(f);
+    setTimer.disabled = true;
+    alert('Timer set for '.concat(seconds.value,' seconds.'));
+    seconds.value=''
+	}
+  });
 
-
-  }, false);
+  }
