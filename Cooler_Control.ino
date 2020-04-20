baf9a9c090023c3e8abbdfeafa5ed228f746c801
Gopalji Gaur <gopaljigaur@gmail.com>
2020-04-20 23:16:14 +0530

    Cleaned up code

--- Cooler_Control.ino
+++ Cooler_Control.ino
@@ -23,15 +23,24 @@ void handleRoot();
 void handleONRequest();
 void handleOFFRequest();
 void handleTimer(long);
-void handleGenericArgs();
-void handleSpecificArg();
+void handleSetTimer();
 void writeFile(long);
-long readData();
+long readFile();
 void cancel();
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  SPIFFS.begin();
+  //Set pin state to value from previous session
+  pinMode(rel, OUTPUT);
+  rcv = readFile();
+  Serial.print("Status from last session : ");
+  Serial.println(stat);
+  if (stat==0)
+    digitalWrite(rel, LOW);
+  else
+    digitalWrite(rel,HIGH);
+  //Start WIFI
   Serial.println('\n');
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP(ssid, pass);
@@ -49,37 +58,22 @@ void setup() {
   IPAddress ipadr = WiFi.softAPIP();
   Serial.print("Hotspot IP: ");
   Serial.println(ipadr);
-
-  pinMode(rel, OUTPUT);
-
   Serial.println("");
-
+  
+  //Associate requests to functions
   server.on("/", handleRoot);
   server.on("/on", handleONRequest);
   server.on("/off", handleOFFRequest);
-  server.on("/genericArgs", handleGenericArgs); //Associate the handler function to the path
-  server.on("/specificArgs", handleSpecificArg);   //Associate the handler function to the path
+  server.on("/setTimer", handleSetTimer);
   server.on("/cancel", cancel);
   server.on("/status", sendStatus);
-  SPIFFS.begin();
 
   server.begin();
   Serial.println("HTTP server started on port 80");
-
-  rcv = readData();
-  Serial.print("Status from last session : ");
-  Serial.println(stat);
-  if (stat==0)
-    digitalWrite(rel, LOW);
-  else
-    digitalWrite(rel,HIGH);
+  //Set timer if time left from previous session was>0
   if (rcv > 0) {
     Serial.print("Time left from previous session in secs : ");
     Serial.println(rcv);
-    //if (stat == 0)
-      //digitalWrite(rel, LOW);
-    //else
-      //digitalWrite(rel, HIGH);
     handleTimer(rcv);
   }
 }
@@ -89,12 +83,12 @@ void loop() {
 }
 
 void handleRoot() {
-  server.send(897, "text/html", "Server up and running");
+  server.send(200, "text/html", "Server up and running");
   Serial.println("handleRoot() was called");
 }
 
 void handleONRequest() {
-  server.send(897, "text/html", "This returns ON status");
+  server.send(200, "text/html", "This returns ON status");
   digitalWrite(rel, HIGH);
   Serial.println("handleONRequest() was called");
   stat = 1; no_need = 1; rcv = 0;
@@ -102,11 +96,83 @@ void handleONRequest() {
 }
 
 void handleOFFRequest() {
-  server.send(897, "text/html", "This returns OFF status");
+  server.send(200, "text/html", "This returns OFF status");
   digitalWrite(rel, LOW);
   Serial.println("handleOFFRequest() was called");
   stat = 0; no_need = 1; rcv = 0;
   writeFile(rcv);
+}
+
+void handleSetTimer() {
+  Serial.println("handleSpecificArgs() was called");
+  no_need = 0;
+  String message = "";
+  String tme = server.arg("secs");
+  if (tme == "") {
+    message = "Time left argument not found (secs) ";
+  }
+  else {
+    message = "Time Left in secs = ";
+    message += tme;
+  }
+
+  server.send(200, "text/html", message);
+  long current_millis = millis();
+  long time_left = (atol(tme.c_str()));
+  time_left = time_left * 1000;
+  time_left = time_left + current_millis;
+  handleTimer(time_left / 1000);
+}
+
+void cancel() {
+  server.send(200, "text/plain", "Timer has been cancelled");
+  Serial.println("cancel() was called");
+  no_need = 1; rcv = 0;
+  writeFile(0);
+}
+
+void sendStatus() {
+  String send_status = String(rcv) + " " + String(stat) + " ";
+  server.send(200, "text/plain", send_status);
+}
+
+long readFile() {
+  String left, pin_status;
+  long rcvd;
+  File f = SPIFFS.open("/timer.txt", "r");
+  while (f.available()) {
+    left = f.readString();
+    int ind = left.indexOf(" ");
+    int len = left.length();
+    pin_status = left.substring(ind + 1, len );
+    left = left.substring(0, ind);
+    //Serial.println("left "+left);
+    //Serial.println("pin_status "+pin_status);
+  }
+  f.close();
+  rcvd = atol(left.c_str());
+  stat = pin_status.toInt();
+  return rcvd;
+}
+
+void writeFile(long left) {
+  Serial.print("Writefile was called with value in secs ");
+  Serial.println(left);
+  File fw = SPIFFS.open("/timer.txt", "w");
+  if (!fw) {
+    Serial.println("Unable to open File");
+  }
+  else
+  {
+    fw.print(left);
+    fw.print(" ");
+    fw.print(stat);
+    Serial.print("wrote : ");
+    Serial.print(left);
+    Serial.print(" ");
+    Serial.println(stat);
+  }
+  fw.close();
 }
 
 void handleTimer(long secs) {
@@ -140,91 +206,4 @@ void handleTimer(long secs) {
     else
       handleONRequest();
   }
-}
-
-void handleGenericArgs() {
-
-  String message = "Number of args received:";
-  message += server.args();
-  message += "\n";
-
-  for (int i = 0; i < server.args(); i++) {
-    message += "Arg no." + (String)i + " –> ";
-    message += server.argName(i) + ": ";
-    message += server.arg(i) + "\n";
-  }
-
-  server.send(897, "text/html", message);
-}
-
-void handleSpecificArg() {
-  Serial.println("handleSpecificArgs() was called");
-  no_need = 0;
-  String message = "";
-  String tme = server.arg("secs");
-  if (tme == "") {
-    message = "Time left argument not found (secs) ";
-  }
-  else {
-    message = "Time Left in secs = ";
-    message += tme;
-  }
-
-  server.send(897, "text/html", message);          //Returns the HTTP response
-  long current_millis = millis();
-  long time_left = (atol(tme.c_str()));
-  time_left = time_left * 1000;
-  time_left = time_left + current_millis;
-  handleTimer(time_left / 1000);
-}
-
-void writeFile(long left) {
-  Serial.print("Writefile was called with value in secs ");
-  Serial.println(left);
-  File fw = SPIFFS.open("/timer.txt", "w");
-  if (!fw) {
-    Serial.println("Unable to open File");
-  }
-  else
-  {
-    fw.print(left);
-    fw.print(" ");
-    fw.print(stat);
-    Serial.print("wrote : ");
-    Serial.print(left);
-    Serial.print(" ");
-    Serial.println(stat);
-  }
-  fw.close();
-}
-
-long readData() {
-  String left, pin_status;
-  long rcvd;
-  File f = SPIFFS.open("/timer.txt", "r");
-  while (f.available()) {
-    left = f.readString();
-    int ind = left.indexOf(" ");
-    int len = left.length();
-    pin_status = left.substring(ind + 1, len );
-    left = left.substring(0, ind);
-    //Serial.println("left "+left);
-    //Serial.println("pin_status "+pin_status);
-  }
-  f.close();
-  rcvd = atol(left.c_str());
-  stat = pin_status.toInt();
-  return rcvd;
-}
-
-void cancel() {
-  server.send(897, "text/plain", "Timer has been cancelled");
-  Serial.println("cancel() was called");
-  no_need = 1; rcv = 0;
-  writeFile(0);
-}
-
-void sendStatus() {
-  String send_status = String(rcv) + " " + String(stat) + " ";
-  server.send(897, "text/plain", send_status);
 }
